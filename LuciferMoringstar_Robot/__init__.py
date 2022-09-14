6c75266faeaac3d0a41822f570d8ac3cad1d8f2c
ᎥᑎᗷᒪᎥ乙 <108299646+Inblizbot@users.noreply.github.com>
2022-09-14 19:34:37 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,13 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  SADIE MOL
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [MUZAFIR](https://t.me/mnzks)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Owner : [I N B L I Z](https://t.me/inbliz)
 """
